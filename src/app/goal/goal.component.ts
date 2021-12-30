67bfc19462179ba0c7bb9fdc6968393e67fcbba6
mohraf <amosngok@gmail.com>
2021-12-30 12:06:56 +0300

    add goal submission

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -26,6 +26,13 @@ export class GoalComponent implements OnInit {
     }
   }
 
+  addNewGoal(goal: Goal) {
+    let goalLength = this.goals.length;
+    goal.id = goalLength + 1;
+    goal.completeDate = new Date(goal.completeDate);
+    this.goals.push(goal);
+  }
+
   deleteGoal(isComplete: boolean, index: number) {
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
